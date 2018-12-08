8d047b0bb6f63b55952ad209358ff2fea93f9ca9
MURAKI Taichi <muraki.taichi@gmail.com>
2018-12-08 20:25:00 +0900

    Add project packages

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
